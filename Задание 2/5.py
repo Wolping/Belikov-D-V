4202899216d1ad097174c84be938d2f1cceafb1e
Wolping <90902590+Wolping@users.noreply.github.com>
2021-09-29 19:56:17 +0300

    Delete Задание 2 directory

--- Задание 2/5.py
+++
@@ -1,8 +0,0 @@
-# -- coding: utf-8 --
-
-a = int(input("Первое число:"))
-b = int(input("Второе число:"))
-c = int(input("Третье число:"))
-def naimenshee(a , b , c):
-	return(min(a,b,c))
-print("Наименьшим числом является:", naimenshee(a,b,c))
--- Задание 2/7.py
+++
@@ -1,7 +0,0 @@
-# -- coding: utf-8 --
-
-g = int(input("Введите год"))
-if (g % 4 == 0 and g % 100 != 0) or g % 400 == 0:
-    print("Да")
-else:
-    print("Нет")
