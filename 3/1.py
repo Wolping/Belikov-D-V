48aece4b2b5c8338ef3a6cd1fbe50ec869234e4a
Wolping <90902590+Wolping@users.noreply.github.com>
2021-10-28 16:50:26 +0300

    Delete 3 directory

--- 3/1.py
+++
@@ -1,7 +0,0 @@
-# -- coding: utf-8 --
-def z():
-    a = int(input())
-    b = int(input())
-    for i in range (a, b+1):
-        print(i)
-z()
--- 3/4.py
+++
@@ -1,8 +0,0 @@
-# -- coding: utf-8 --
-def z():
-    n = int(input())
-    s = 0
-    for i in range (n):
-        s = s + int(input())
-    print (s)
-z()
