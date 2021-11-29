4520983a632d451dd365296bc1aabe1f0d064330
Wolping <90902590+Wolping@users.noreply.github.com>
2021-11-29 10:11:07 +0300

    Add files via upload

---
+++ Задание 5/1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+def m():
+    a = 0
+    b = int(input("Введите число - "))
+    while a <= b:
+        print(a * a)
+        a+=1
+
+m()
---
+++ Задание 5/5.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+def m():
+    a = 0
+    while int(input("Введите число - ")) != 0:
+        a+=1
+    print(a)
+
+m()
